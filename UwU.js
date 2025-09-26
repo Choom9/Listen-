bd7686e7c55bfe1e8b45b481a05d1dcc8c5e53b1
Nikos Meletlidis <nikosmeletlidis@MacBook-Air-von-Nikos.fritz.box>
2025-09-27 01:37:42 +0200

    2

--- UwU.js
+++ UwU.js
@@ -1,8 +1,18 @@
 let akkorde = [
     "Amoll", "Cdur", "Ddur", "Emoll", "FDur", "Gdur"
 ]
-const playSound = function() {
-    let akkord = Math.floor(Math.random()*akkorde.length)
-    return akkord
+let akkord
+let playSound = function() {
+    akkord = Math.floor(Math.random()*akkorde.length);
+    console.log(akkord);
 }
-console.log(playSound())
+let checkeAkkord= function(rAkkord) {
+    if (rAkkord == akkord) {
+        true;
+        console.log("Richtig !")
+    }  
+    else {
+        false 
+        console.log("Falsch !")
+    }
+}
